4f05f76c54d4cf38d7958a6c9f657638c435c8a8
Frank Mitarotonda <Frank.Mitarotonda@marist.edu>
2016-01-27 23:54:10 -0500

    Build Documentation
    Overview of my Compiler’s design and goals

---
+++ untitled text.js
@@ -0,0 +1,17 @@
+//Author: Frank Mitarotonda 
+//Date: January 27th 2016
+//Instructor: Professor Labouseur
+//Course: Compilers CMPT_432
+/*This document's purpose is to outline and describe the structure of
+ my compiler and its functions. As well as describe the documentation style used 
+ in my code. I entail for my compiler to be written in 
+ JavaScript and as result I anticipate it to run within any type of
+ web browser. Like all compilers, I intend for my compiler to efficiently
+ and successfully translate high level human readable computer languages into 
+ machine readable code for execution. The backbone of my compiler will rely
+ heavily on the grammar and the semantics that I define which will map out how 
+ the compiler handles the translation of high level coding languages. By
+ developing a strong grammar that the parser can recognize and validate this 
+ compiler will have a good foundation thereby contributing to its efficient ability
+ to translate human readable programming languages.     
+ */ 
