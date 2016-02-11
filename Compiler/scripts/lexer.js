48ace9e88f63423efafab5850f79fd191ac08377
Frank Mitarotonda <Frank.Mitarotonda@marist.edu>
2016-02-10 19:19:39 -0500

    Lear Part 1
    Made lever examine each character of source code entered into the
    compiler

--- Compiler/scripts/lexer.js
+++ Compiler/scripts/lexer.js
@@ -7,7 +7,15 @@
         // Trim the leading and trailing spaces.
         sourceCode = trim(sourceCode);
         //Declare Tokens as incoming source code
-        putMessage("Lex returned [" +sourceCode+ "]");
-           
+        var tokens = sourceCode.split("");
+        //TODO: remove all spaces in the middle; remove line breaks too.
+        //var keywords = ["while", "if", "print", "int","string","boolean"];
+        //var pattern = /^[a-zA-Z]*$/
+        //var pattern = /^[while if]*$/
+        var lexem = tokens.length;
+        for (var i = 0; i < lexem; i++) {
+        var tokenval = tokens[i];
+        putMessage("Lex returned [" + tokenval + "]");
+           }
     }
 
